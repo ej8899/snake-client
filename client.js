490efb69944d149d7dc6666d9cad69e7fe0d234b
ej8899 <ej8899@gmail.com>
2022-07-24 14:12:45 -0600

    setup as an include file

--- client.js
+++ client.js
@@ -1,6 +1,19 @@
+
 // establishes a connection with the game server
-const connect = function () {
+const net = require("net");
+const connects = function () {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541,
+    port: 50541
   });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+module.exports = {
+  connect: connects,
+};
