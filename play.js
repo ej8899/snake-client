4b4f7f0687477869ef4fbdbfd6bae6e0a499ef5b
ej8899 <ej8899@gmail.com>
2022-07-23 19:19:07 -0600

    client.js setup

--- play.js
+++ play.js
@@ -1,11 +1,6 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
