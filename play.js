7d849d81e17ee71b1c4f3035ef81932fba5b2672
ej8899 <ej8899@gmail.com>
2022-07-28 08:23:32 -0600

    capture connect error and give a nice exit

--- play.js
+++ play.js
@@ -4,24 +4,22 @@ const globalVars = require("./globalVars.js");
 
 
 console.clear();
-console.log(globalVars.conColors.conColorDim + "Welcome to..." + globalVars.conColors.conColorReset + globalVars.conColors.conColorGreen);
-
-let conn = connect();
-
-
-console.log (`     _______..__   __.     ___      __  ___  _______   ${globalVars.conColors.conColorRed}__${globalVars.conColors.conColorGreen}  `);
-console.log (`    /       ||  \\ |  |    /   \\    |  |/  / |   ____| ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
-console.log (`   |   (----\\|   \\|  |   /  ^  \\   |  '  /  |  |__    ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
-console.log (`    \\   \\    |  . \\  |  /  /_\\  \\  |    <   |   __|   ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
-console.log (`.----)   |   |  |\\   | /  _____  \\ |  .  \\  |  |____  ${globalVars.conColors.conColorRed}|__|${globalVars.conColors.conColorGreen} `);
-console.log (`|_______/    |__| \\__| __/     \\__\\ __|\\__\\ |_______| ${globalVars.conColors.conColorRed}(__)${globalVars.conColors.conColorGreen} `);
+console.log(globalVars.conColors.conColorDim + " Welcome to..." + globalVars.conColors.conColorReset + globalVars.conColors.conColorGreen);
+console.log (`      _______..__   __.     ___      __  ___  _______   ${globalVars.conColors.conColorRed}__${globalVars.conColors.conColorGreen}  `);
+console.log (`     /       ||  \\ |  |    /   \\    |  |/  / |   ____| ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
+console.log (`    |   (----\\|   \\|  |   /  ^  \\   |  '  /  |  |__    ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
+console.log (`     \\   \\    |  . \\  |  /  /_\\  \\  |    <   |   __|   ${globalVars.conColors.conColorRed}|  |${globalVars.conColors.conColorGreen} `);
+console.log (` .----)   |   |  |\\   | /  _____  \\ |  .  \\  |  |____  ${globalVars.conColors.conColorRed}|__|${globalVars.conColors.conColorGreen} `);
+console.log (` |_______/    |__| \\__| __/     \\__\\ __|\\__\\ |_______| ${globalVars.conColors.conColorRed}(__)${globalVars.conColors.conColorGreen} `);
 console.log(globalVars.conColors.conColorReset);
 
 
 globalVars.gameStartTimeInSeconds = globalVars.setGameStartTime(); // start the clock on our game
+
+let conn = connect();
+
 setupInput(conn);
 
 
-
+// !TODO:  error grab if snake server isn't running
 // !TODO:  keep the snake moving after each keypress until the next keypress
-// !TODO:  if directions changed are 0 tell the user they didn't even play - show the instructions
