b61959bbad44899313098e0d8d4c30f0712b2416
ej8899 <ej8899@gmail.com>
2022-07-28 15:14:14 -0600

    comments

--- globalfn.js
+++ globalfn.js
@@ -1,5 +1,13 @@
+//
+// Global functions
+//
+
+
 const globalVars = require("./globalVars.js");
 
+//
+// show an end game summary and their 'scoring' based on seconds played & moves made
+//
 const endGameSummary = (secondsplaying) => {
   console.log(globalVars.conColors.conColorGreen + "          You made " + globalVars.directionsCounter + " moves, and lasted " + secondsplaying + " seconds!" + globalVars.conColors.conColorReset);
   console.log(globalVars.conColors.conColorDim + globalVars.conColors.conColorRed + '-------------------------------------------------------------' + globalVars.conColors.conColorReset);
